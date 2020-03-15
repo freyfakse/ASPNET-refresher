1daae08e879a2c9639a47fff0d1c55e3026b69e1
Jonas Claesson <jonasc15@student.uia.no>
2020-03-15 13:42:54 +0100

    adjuster _layout, added pages for dnd projects

---
+++ WebApplication1/Controllers/DNDController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DNDController : Controller
+    {
+        // GET: DND
+        public ActionResult NameGenerator()
+        {
+            ViewBag.Message = "Name generator";
+            return View();
+        }
+
+        public ActionResult AbilityScoreGenerator()
+        {
+            ViewBag.Message = "Ability score generator";
+            return View();
+        }
+    }
+}
